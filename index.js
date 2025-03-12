6b2859c7714f44d62124349d1d08efbf9bd42d57
Кравец Дмитрий <kravec@kinoplan.tech>
2025-03-12 14:08:48 +0300

    add env and vercel.json

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 import express from "express";
 
-const PORT = 8070
+// export PORT = 9999 && node index.js
+const PORT = process.env.PORT ?? 8070
 
 const app = express();
 
 app.use(express.json());
 
 let bodyArr = [];
+let requestsArr = [];
+let postRequestsCount = 0;
 
 app.post("/csp_report", (req, res) => {
 	console.log("Post csp req:", req);
+
 	bodyArr.push(req.body);
+	requestsArr.push(req.host + "/" + req.url);
+	postRequestsCount += 1;
 
 	res.status(204).end();
 });
 
 app.get("/csp_report", (req, res) => {
-	res.status(200).json({message: "/csp/report", data: bodyArr})
+	res.status(200).json({message: "/csp/report", data: {
+		bodyArr,
+		urls: requestsArr,
+		postRequestsCount
+	}})
 });
 
 app.listen(PORT, () => {
